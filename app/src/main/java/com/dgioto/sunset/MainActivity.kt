69cb04afb5da1cb7e92f483604ceb242b8cd90ad
Alexander Tih <dgioto@gmail.com>
2022-06-30 12:43:27 +0300

    Изменение цвета

--- app/src/main/java/com/dgioto/sunset/MainActivity.kt
+++ app/src/main/java/com/dgioto/sunset/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.dgioto.sunset
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateInterpolator
+import androidx.core.content.ContextCompat
 
-private lateinit var sceneView: View
-private lateinit var sunView: View
-private lateinit var skyView: View
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sceneView: View
+    private lateinit var sunView: View
+    private lateinit var skyView: View
+
+    //Загрузка закатных цветов
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,6 +53,15 @@ class MainActivity : AppCompatActivity() {
         //Добавление ускорения
         heightAnimator.interpolator = AccelerateInterpolator()
 
+        //Анимация цвета неба
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+
+        //Назначение ArgbEvaluator
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
         heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
